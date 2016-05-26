9173d654716ebdde36bbfe6646fe545795e18577
Minh Van <mvan89@yahoo.com>
2016-05-26 15:58:19 -0700

    adding carousel to front page

--- client/controllers/dashboard_controller.js
+++ client/controllers/dashboard_controller.js
@@ -55,5 +55,20 @@ setInterval(function () {
   // take a breath.. hold event listener from firing for 100ms
 }, 100);
 
+  //carousel slides below. 
+  $scope.myInterval = 3000;
+
+      $scope.slides = [
+        {
+          image: 'http://images.performgroup.com/di/library/sporting_news/6c/3a/kobe-11_rwuzvx4yp7mx1bxmbufdo09s9.jpg?w=1920&h=1080&quality=100'
+        },
+        {
+          image: 'http://sneakerhdwallpapers.com/wallpapers/2015/lebron-13-1920x1080-wallpaper.jpg'
+        },
+        {
+          image: 'http://images.performgroup.com/di/library/sporting_news/f7/a8/kevin-durant-kd8-062315-ftr-nikejpg_1xhn93vs5u93t1g31d4pokz6x8.jpg?ggnoads'
+        },
+      ];
+
 
 });
